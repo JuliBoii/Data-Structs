392f38abf9276e19318f53955e5f4785dd691702
JuliBoii <juliboii1323@gmail.com>
2024-10-07 13:25:26 -0700

    Set up nodes for Tree DS

---
+++ structsCPP/Node/TreeNode.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Angel Hernandez on 10/7/24.
+//
+
+#include "TreeNode.hpp"
+
+TreeNode::TreeNode(const int& value) {
+    this->value = value;
+    this->left = nullptr;
+    this->right = nullptr;
+}
---
+++ structsCPP/Node/TreeNode.hpp
@@ -0,0 +1,21 @@
+//
+// Created by Angel Hernandez on 10/7/24.
+//
+
+#ifndef TREENODE_HPP
+#define TREENODE_HPP
+
+
+
+class TreeNode {
+    int value;
+    TreeNode *left;
+    TreeNode *right;
+
+    public:
+      TreeNode(const int& value);
+};
+
+
+
+#endif //TREENODE_HPP
