c5d3f1a80eb4b86351df3e0908517af4ade4bd16
JuliBoii <juliboii1323@gmail.com>
2024-10-11 19:56:37 -0700

    LL for Python setup

--- structsPYTHON/Lists/LinkedList/LinkedList.py
+++ structsPYTHON/Lists/LinkedList/LinkedList.py
@@ -1,8 +1,10 @@
+from typing import Any
+
+
 class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
-
 
 class LinkedList:
     def __init__(self, value):
@@ -11,17 +13,116 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
-    def append(self, value):
+    def append(self, value) -> None:
+        new_node = Node(value)
+
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+        self.length += 1
+
+    def prepend(self, value) -> None:
+        new_node = Node(value)
+
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+
+        self.length += 1
+
+    def insert(self, index, value) -> bool:
+        if index < 0 or index > self.length:
+            return False
+        if self.length == 0:
+            self.prepend(value)
+            return True
+        if index == self.length - 1:
+            self.append(value)
+            return True
+
+        new_node = Node(value)
+        # temp =
+
+    def pop_first(self) -> Node | None:
+        if self.length == 0:
+            return None
+
+        temp_node = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            temp_node.next = None
+
+        self.length -= 1
+        return temp_node
+
+    def pop(self) -> Node | None:
+        if self.length == 0:
+            return None
+
+        temp_node = self.head
+
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            while temp_node.next.next is not None:
+                temp_node = temp_node.next
+
+            self.tail = temp_node
+            temp_node = temp_node.next
+            self.tail.next = None
+
+        self.length -= 1
+        return temp_node
+
+    def remove(self, index): # deleteNode
         pass
 
-    def prepend(self, value):
+    def print(self) -> None | Any:
+        if self.length == 0:
+            print("List is empty")
+            return
+
+        temp = self.head
+
+        while temp:
+            print(f"{temp.value}")
+            temp = temp.next
+
+    def set_value(self, index, value) -> None:
         pass
 
-    def insert(self, index, value):
-        pass
+    def get_node(self, index) -> Node | None:
+        if self.length == 0:
+            return None
+        if index < 0 or index >= self.length:
+            return None
 
-    def pop(self):
-        pass
+        count = 0
+        temp_node = self.head
+        while count < index:
+            temp_node = temp_node.next
+            count += 1
 
-    def remove(self, index):
-        pass
+        return temp_node
+
+    def get_head(self) -> int:
+        return self.head.value
+
+    def get_tail(self) -> int:
+        return self.tail.value
+
+    def get_length(self) -> int:
+        return self.length
+
+
--- structsPYTHON/Lists/main.py
+++ structsPYTHON/Lists/main.py
@@ -2,3 +2,35 @@ from LinkedList.LinkedList import LinkedList
 
 if __name__ == "__main__":
     linked_list = LinkedList(4)
+
+    print("Printing List")
+    linked_list.print()
+
+    print("\nAppending a Node...")
+    linked_list.append(-852)
+
+    print("Prepending two Nodes...")
+    linked_list.prepend(442)
+    linked_list.prepend(40)
+
+    print("Printing List")
+    linked_list.print()
+
+    print("\nPopping two Nodes...")
+    linked_list.pop()
+    linked_list.pop()
+
+    print("Prepending two Nodes...")
+    linked_list.prepend(25)
+    linked_list.prepend(5)
+
+    print("Printing List")
+    linked_list.print()
+
+    print("\nPopping three Nodes from beginning of List...")
+    print(linked_list.pop_first())
+    print(linked_list.pop_first())
+    print(linked_list.pop_first())
+
+    print("\nPrinting List")
+    linked_list.print()
